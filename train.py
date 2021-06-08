0c5b827324c1da21cb490bf2fbf88fa7ce5305cc
Michal Karol <michal.p.karol@gmail.com>
2021-06-08 03:40:12 +0200

    Adjust

--- train.py
+++ train.py
@@ -554,6 +554,10 @@ def get_args(**kwargs):
         '--keep-checkpoint-max', type=int, default=10,
         help='maximum number of checkpoints to keep. If set 0, all checkpoints will be kept',
         dest='keep_checkpoint_max')
+    parser.add_argument(
+        '--checkpoints', type=str, default="./checkpoints",
+        help='checkpoints dir',
+        dest='checkpoints')
     args = vars(parser.parse_args())
 
     # for k in args.keys():
